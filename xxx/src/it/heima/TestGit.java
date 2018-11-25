5489afbbc4aa40d7afa2cc2707f7100a9e4d8558
zhangsan <zhangsan@qq.com>
2018-11-25 19:41:00 +0800

    提交工程文件

---
+++ xxx/src/it/heima/TestGit.java
@@ -0,0 +1,11 @@
+package it.heima;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello github ...");
+        System.out.println("hello github ...");
+        System.out.println("hello github ...");
+        System.out.println("hello github ...");
+        System.out.println("hello github ...");
+    }
+}
